357219ef4405dcd1777e1ba362490996ee10120d
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-03-15 18:32:22 +0000

    feat: use templates in webapp routes via dependency injection
    Add a 'get_templates()' dependency to return a Jinja templates
    environment. Use this in the root webapp endpoint and render
    a response using the 'main' template.

--- depositduck/dependencies.py
+++ depositduck/dependencies.py
@@ -3,10 +3,21 @@
 """
 
 from functools import lru_cache
+from pathlib import Path
+
+from fastapi.templating import Jinja2Templates
 
 from depositduck import config
+
+BASE_DIR = Path(__file__).resolve().parent
 
 
 @lru_cache
 def get_settings():
     return config.Settings()
+
+
+@lru_cache
+def get_templates():
+    templates_dir_path = BASE_DIR / "web" / "templates"
+    return Jinja2Templates(directory=str(templates_dir_path))
--- depositduck/web/routes.py
+++ depositduck/web/routes.py
@@ -2,11 +2,12 @@
 (c) 2024 Alberto Morón Hernández
 """
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
+from fastapi.templating import Jinja2Templates
 from typing_extensions import Annotated
 
 from depositduck import config
-from depositduck.dependencies import get_settings
+from depositduck.dependencies import get_settings, get_templates
 
 web_router = APIRouter()
 
@@ -14,5 +15,10 @@ web_router = APIRouter()
 @web_router.get("/")
 async def root(
     settings: Annotated[config.Settings, Depends(get_settings)],
+    templates: Annotated[Jinja2Templates, Depends(get_templates)],
+    request: Request,
 ):
-    return f"app: {settings.app_name}"
+    context = {"app_name": settings.app_name}
+    return templates.TemplateResponse(
+        request=request, name="main.html.jinja2", context=context
+    )
