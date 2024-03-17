bbf387bab77d28985e7a2e6a0835124cea60945d
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-03-17 19:05:29 +0000

    feat: configure web & api apps using settings dependable

---
+++ depositduck/__init__.py
@@ -0,0 +1,7 @@
+"""
+(c) 2024 Alberto Morón Hernández
+"""
+
+VERSION_MAJOR = 0
+VERSION_MINOR = 1
+VERSION_PATCH = 0
--- depositduck/web/routes.py
+++ depositduck/web/routes.py
@@ -3,7 +3,6 @@
 """
 
 from fastapi import APIRouter, Depends, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from typing_extensions import Annotated
 
@@ -13,7 +12,7 @@ from depositduck.dependables import get_settings, get_templates
 web_router = APIRouter()
 
 
-@web_router.get("/", response_class=HTMLResponse)
+@web_router.get("/")
 async def root(
     settings: Annotated[config.Settings, Depends(get_settings)],
     templates: Annotated[Jinja2Templates, Depends(get_templates)],
@@ -26,7 +25,7 @@ async def root(
     return templates.TemplateResponse("home.html.jinja2", context)
 
 
-@web_router.get("/motd", response_class=HTMLResponse)
+@web_router.get("/motd")
 async def get_motd(
     templates: Annotated[Jinja2Templates, Depends(get_templates)],
     request: Request,
@@ -37,7 +36,7 @@ async def get_motd(
     )
 
 
-@web_router.get("/fragment", response_class=HTMLResponse)
+@web_router.get("/fragment")
 async def get_fragment(
     templates: Annotated[Jinja2Templates, Depends(get_templates)],
     request: Request,
