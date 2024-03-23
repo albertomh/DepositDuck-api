98bc5b680fc462f131dfd0e5268a6729cb5853a1
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-03-23 14:38:26 +0000

    feat: add an endpoint to record SourceTexts

--- depositduck/llm/routes.py
+++ depositduck/llm/routes.py
@@ -2,19 +2,31 @@
 (c) 2024 Alberto Morón Hernández
 """
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from sqlmodel.ext.asyncio.session import AsyncSession
 from typing_extensions import Annotated
 
-from depositduck.dependables import get_settings
-from depositduck.settings import Settings
+from depositduck.dependables import get_db_session
+from depositduck.models.dto.llm import SourceTextCreate
+from depositduck.models.sql.llm import SourceText
 
 llm_router = APIRouter()
 
 
-@llm_router.get("/")
-async def root(
-    settings: Annotated[Settings, Depends(get_settings)],
-    request: Request,
+@llm_router.post("/SourceText")
+async def create_source_text(
+    source_text: SourceTextCreate,
+    db_session: Annotated[AsyncSession, Depends(get_db_session)],
 ):
-    # TODO: stub
-    return {}
+    data = source_text.model_dump()
+    new_text = SourceText(**data)
+
+    try:
+        db_session.add(new_text)
+        await db_session.commit()
+        await db_session.refresh(new_text)
+        return new_text
+
+    except (IntegrityError, InvalidRequestError):
+        await db_session.rollback()
