303f188645254df65812fa6b000766789cbb2ad3
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-03-23 07:13:16 +0000

    refactor: move SQLModel mixins and table class to dedicated module

---
+++ depositduck/models/common.py
@@ -0,0 +1,38 @@
+"""
+SQLModel derived classes and mixins to help build base models and tables in the
+context-specific modules (auth, LLM, etc.) in this package.
+
+(c) 2024 Alberto Morón Hernández
+"""
+
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import Column, DateTime, func
+from sqlmodel import Field, SQLModel
+
+
+class IdMixin:
+    id: UUID = Field(
+        primary_key=True,
+        sa_column_kwargs=dict(server_default=func.gen_random_uuid()),
+    )
+
+
+class CreatedAtMixin:
+    created_at: datetime = Field(sa_column_kwargs=dict(server_default=func.now()))
+
+
+# TODO: created_by
+# updated_at, updated_by, etc. should be inferred from an `audit` table
+# updated_at: datetime | None = Field(
+#     sa_column=Column(DateTime(timezone=True), onupdate=func.now())
+# )
+
+
+class DeletedAtMixin:
+    deleted_at: datetime | None = Field(sa_column=Column(DateTime(timezone=True)))
+
+
+class TableBase(SQLModel, IdMixin, CreatedAtMixin, DeletedAtMixin):
+    pass
