2e1ff82b05dc225b3cd8eada98e610df66834c00
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-03-23 12:39:36 +0000

    feat: add Snippet model & table to store source material for embeddings or context windows

--- depositduck/models/sql/llm.py
+++ depositduck/models/sql/llm.py
@@ -4,19 +4,34 @@ Database tables to keep track of which LLMs are in use and store embeddings.
 (c) 2024 Alberto Morón Hernández
 """
 
+from uuid import UUID
+
 from pgvector.sqlalchemy import Vector
 from sqlalchemy import Column
 from sqlmodel import Field, Relationship, SQLModel
 
 from depositduck.models.common import CreatedAtMixin, DeletedAtMixin, IdMixin, TableBase
-from depositduck.models.llm import AvailableLLM, LLMBase, SourceTextBase
+from depositduck.models.llm import (
+    AvailableLLM,
+    EmbeddingMiniLML6V2Base,
+    LLMBase,
+    SnippetBase,
+    SourceTextBase,
+)
 
 
 class SourceText(SourceTextBase, TableBase, table=True):
     __tablename__ = "llm__source_text"
 
 
-class LLM(LLMBase, CreatedAtMixin, DeletedAtMixin, SQLModel, table=True):
+class Snippet(SnippetBase, TableBase, table=True):
+    __tablename__ = "llm__snippet"
+
+    source_text_id: UUID = Field(default=None, foreign_key="llm__source_text.id")
+    source_text: SourceText = Relationship(back_populates="snippets")
+
+
+class LLM(SQLModel, LLMBase, CreatedAtMixin, DeletedAtMixin, table=True):
     """
     The source of truth for LLMs available now or used previously is the AvailableLLM
     enum. We must keep track of these LLM options in the database in order to
@@ -33,10 +48,11 @@ class LLM(LLMBase, CreatedAtMixin, DeletedAtMixin, SQLModel, table=True):
 # tables that have an appropriately-sized vector column.
 
 
-class EmbeddingMiniLML6V2(SQLModel, IdMixin, CreatedAtMixin, table=True):
+class EmbeddingMiniLML6V2(
+    SQLModel, EmbeddingMiniLML6V2Base, IdMixin, CreatedAtMixin, table=True
+):
     __tablename__ = "llm__embedding_minilm_l6_v2"
 
-    text: str
     vector: list[float] = Field(
         sa_column=Column(
             Vector(AvailableLLM.MINILM_L6_V2.value.dimensions), nullable=False
