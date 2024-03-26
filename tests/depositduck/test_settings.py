36011a3401178b46fa8880e59d4607bcb953c788
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-03-26 23:12:13 +0000

    refactor: simplify dynamically picking llm.embedding_model Setting

--- tests/depositduck/test_settings.py
+++ tests/depositduck/test_settings.py
@@ -4,23 +4,25 @@
 
 import pytest
 
-from depositduck.dependables import get_settings
 from depositduck.settings import AvailableLLM, Settings
 
 
-def test_valid_llm_settings():
-    settings = get_settings()
+def test_valid_llm_settings(monkeypatch):
+    monkeypatch.setenv("LLM__EMBEDDING_MODEL_NAME", "multi-qa-MiniLM-L6-cos-v1")
 
-    minilm = AvailableLLM.MINILM_L6_V2
-    assert settings.llm.embedding_model.name == minilm.value.name
-    assert settings.llm.embedding_model.dimensions == minilm.value.dimensions
+    settings = Settings()
+
+    minilm = AvailableLLM.MULTI_QA_MINILM_L6_COS_V1
+    assert settings.llm.embedding_model.value.name == minilm.value.name
+    assert settings.llm.embedding_model.value.dimensions == minilm.value.dimensions
 
 
 def test_missing_llm_model_name(monkeypatch):
-    monkeypatch.setenv("LLM__EMBEDDING_MODEL_NAME", "")
+    invalid_model_name = ""
+    monkeypatch.setenv("LLM__EMBEDDING_MODEL_NAME", invalid_model_name)
 
     with pytest.raises(ValueError) as exc_info:
         Settings()
 
-    assert "invalid model choice" in str(exc_info)
-    assert "- check LLM__EMBEDDING_MODEL_NAME in .env" in str(exc_info)
+    msg = f"no AvailableLLM found matching 'embedding_model_name={invalid_model_name}"
+    assert msg in str(exc_info)
