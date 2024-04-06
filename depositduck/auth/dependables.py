655bbf924ac04006418196bf58bafa4549ecfd4b
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-06 22:46:33 +0100

    feat: authentication backend using fastapi-users
    Add an auth backend using 1) the database strategy, storing
    access tokens in a table and 2) the cookie transport for
    ease of use since managed automatically by browsers.
    Add a UserManager class to centralize authentication logic.

---
+++ depositduck/auth/dependables.py
@@ -0,0 +1,105 @@
+"""
+UserManager and related dependables using fastapi-users.
+https://fastapi-users.github.io/fastapi-users/13.0/configuration/user-manager/
+
+(c) 2024 Alberto Morón Hernández
+"""
+
+import uuid
+from typing import Annotated, Optional
+
+from fastapi import Depends, Request, Response
+from fastapi_users import BaseUserManager, UUIDIDMixin
+from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+from fastapi_users.db import SQLAlchemyUserDatabase
+from fastapi_users_db_sqlmodel.access_token import SQLModelAccessTokenDatabaseAsync
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from depositduck.dependables import get_db_session, get_logger, get_settings
+from depositduck.models.sql.auth import AccessToken, User
+
+settings = get_settings()
+LOG = get_logger()
+ACCESS_TOKEN_LIFETIME_IN_SECONDS = 3600
+
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    # reset & verification token lifetimes default to 3600 seconds
+    reset_password_token_secret = settings.app_secret
+    verification_token_secret = settings.app_secret
+
+    async def validate_password(
+        self,
+        password: str,
+        user: User,  # type: ignore
+    ) -> None:
+        # TODO: stub - validate length & check isn't common password
+        pass
+
+    async def on_after_register(
+        self,
+        user: User,
+        request: Optional[Request] = None,
+    ):
+        LOG.debug(f"User {user.id} registered")
+
+    async def on_after_login(
+        self,
+        user: User,
+        request: Optional[Request] = None,
+        response: Optional[Response] = None,
+    ):
+        # TODO: stub - save event for analytics
+        LOG.debug(f"User {user.id} logged in")
+
+    async def on_after_request_verify(
+        self,
+        user: User,
+        token: str,
+        request: Optional[Request] = None,
+    ):
+        # TODO: stub - enqueue verification email
+        LOG.debug(f"Verification requested for user {user.id} - token: {token}")
+
+    async def on_after_forgot_password(
+        self,
+        user: User,
+        token: str,
+        request: Optional[Request] = None,
+    ):
+        # TODO: stub - rate limit & enqueue reset email
+        LOG.debug(f"User {user.id} requested a password reset - token: {token}")
+
+    async def on_after_reset_password(
+        self,
+        user: User,
+        request: Optional[Request] = None,
+    ):
+        # TODO: stub - log
+        LOG.debug(f"User {user.id} has reset their password.")
+
+
+async def get_user_db(db_session: Annotated[AsyncSession, Depends(get_db_session)]):
+    yield SQLAlchemyUserDatabase(db_session, User)
+
+
+async def get_user_manager(
+    user_db: Annotated[SQLAlchemyUserDatabase, Depends(get_user_db)],
+):
+    yield UserManager(user_db)
+
+
+async def get_access_token_db(
+    db_session: Annotated[AsyncSession, Depends(get_db_session)],
+):
+    yield SQLModelAccessTokenDatabaseAsync(db_session, AccessToken)
+
+
+def get_database_strategy(
+    access_token_db: Annotated[
+        AccessTokenDatabase[AccessToken], Depends(get_access_token_db)
+    ],
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        access_token_db, lifetime_seconds=ACCESS_TOKEN_LIFETIME_IN_SECONDS
+    )
---
+++ depositduck/auth/users.py
@@ -0,0 +1,52 @@
+"""
+Configure the fastapi-users library to provide an authentication backend.
+
+Using the SQLAlchemy adapter. The auth backend uses:
+- the database strategy: can be invalidated server-side & provides data for analytics.
+- the cookie transport: ol' reliable, automatically managed by browsers
+
+https://fastapi-users.github.io/fastapi-users/13.0/configuration/overview/
+
+(c) 2024 Alberto Morón Hernández
+"""
+
+import uuid
+from typing import Literal
+
+from fastapi_users import FastAPIUsers
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    CookieTransport,
+)
+
+from depositduck.auth.dependables import get_database_strategy, get_user_manager
+from depositduck.dependables import get_settings
+from depositduck.models.sql.auth import User
+
+settings = get_settings()
+COOKIE_MAX_AGE = 3600
+
+cookie_secure = not settings.debug
+cookie_samesite: Literal["lax", "strict"] = "lax" if settings.debug else "strict"
+cookie_httponly = not settings.debug
+cookie_transport = CookieTransport(
+    cookie_name="dd_auth",
+    cookie_max_age=COOKIE_MAX_AGE,
+    # TODO: set cookie_domain for hosted environments when available via Settings
+    # cookie_domain=,
+    cookie_secure=cookie_secure,
+    cookie_httponly=cookie_httponly,  # TODO: might not be needed
+    cookie_samesite=cookie_samesite,
+)
+
+auth_backend = AuthenticationBackend(
+    name="db+cookie",
+    transport=cookie_transport,
+    get_strategy=get_database_strategy,
+)
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
+
+# `optional` to return None instead of directly raising a HttpException
+# `active` to require that the User attempting to log in is `active` in the database
+current_active_user = fastapi_users.current_user(optional=True, active=True)
