920359f31142926e7fd12b2f29a93806334965c5
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-07 23:54:56 +0100

    feat(fe): add validation messages to the sign up screen

--- depositduck/auth/routes.py
+++ depositduck/auth/routes.py
@@ -10,7 +10,6 @@ from fastapi_users.exceptions import InvalidPasswordException, UserAlreadyExists
 from fastapi_users.router.common import ErrorCode
 from jinja2_fragments.fastapi import Jinja2Blocks
 from pydantic import ValidationError
-from structlog._config import BoundLoggerLazyProxy
 from typing_extensions import Annotated
 
 from depositduck.auth.dependables import (
@@ -19,11 +18,26 @@ from depositduck.auth.dependables import (
     get_user_manager,
 )
 from depositduck.auth.users import auth_backend
-from depositduck.dependables import get_logger, get_templates
+from depositduck.dependables import get_templates
 from depositduck.models.auth import UserCreate
+from depositduck.models.sql.auth import User
 
 auth_frontend_router = APIRouter(tags=["auth", "frontend"])
 auth_operations_router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+async def log_user_in_and_redirect(
+    auth_db_strategy: DatabaseStrategy, user: User, redirect_to: str
+) -> Response:
+    """
+    Log a given user in returning a HTTP 302 response with a fresh auth cookie attached
+    to it. Redirect to the specified path.
+    """
+    headers = {"HX-Redirect": redirect_to}
+    response = Response(status_code=status.HTTP_302_FOUND, headers=headers)
+    token = await auth_db_strategy.write_token(user)
+    response = auth_backend.transport._set_login_cookie(response, token)  # type: ignore[attr-defined]
+    return response
 
 
 @auth_frontend_router.get("/signup/")
@@ -37,37 +51,36 @@ async def signup(
     return templates.TemplateResponse("auth/signup.html.jinja2", context)
 
 
-@auth_operations_router.post("/register/", status_code=status.HTTP_201_CREATED)
+@auth_operations_router.post("/register/")
 async def register(
     username: Annotated[str, Form()],
     password: Annotated[str, Form()],
-    user_manager: Annotated[UserManager, Depends(get_user_manager)],
-    LOG: Annotated[BoundLoggerLazyProxy, Depends(get_logger)],
+    auth_db_strategy: Annotated[DatabaseStrategy, Depends(get_database_strategy)],
     request: Request,
+    templates: Annotated[Jinja2Blocks, Depends(get_templates)],
+    user_manager: Annotated[UserManager, Depends(get_user_manager)],
 ):
+    # TODO: redirect user away if already logged in.
+    errors: list[str] = []
     try:
         user_create = UserCreate(email=username, password=password)
         created_user = await user_manager.create(user_create, safe=True, request=request)
-        LOG.debug(created_user.__dict__)
+        redirect_response = await log_user_in_and_redirect(
+            auth_db_strategy, created_user, "/"
+        )
+        return redirect_response
     except UserAlreadyExists:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=ErrorCode.REGISTER_USER_ALREADY_EXISTS,
-        )
+        errors.append(ErrorCode.REGISTER_USER_ALREADY_EXISTS.value)
+        # TODO: redirect user to /login/.
     except InvalidPasswordException as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail={
-                "code": ErrorCode.REGISTER_INVALID_PASSWORD,
-                "reason": e.reason,
-            },
-        )
+        errors.extend([ErrorCode.REGISTER_INVALID_PASSWORD.value, e.reason.value])
     except ValidationError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="REGISTER_INVALID_EMAIL",
-        )
-    return Response(status_code=status.HTTP_201_CREATED)
+        errors.append("REGISTER_INVALID_EMAIL")
+
+    context = dict(request=request, username=username, errors=errors)
+    return templates.TemplateResponse(
+        "auth/signup.html.jinja2", context=context, block_name="signup_form"
+    )
 
 
 @auth_frontend_router.get("/login/")
@@ -84,8 +97,8 @@ async def login(
 @auth_operations_router.post("/authenticate/")
 async def authenticate(
     credentials: Annotated[OAuth2PasswordRequestForm, Depends()],
-    user_manager: Annotated[UserManager, Depends(get_user_manager)],
     auth_db_strategy: Annotated[DatabaseStrategy, Depends(get_database_strategy)],
+    user_manager: Annotated[UserManager, Depends(get_user_manager)],
 ):
     user = await user_manager.authenticate(credentials)
 
@@ -94,14 +107,12 @@ async def authenticate(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=ErrorCode.LOGIN_BAD_CREDENTIALS,
         )
+    # TODO: look at POST /register/ above and follow error-handling HTMX fragments pattern
     # if requires_verification and not user.is_verified:
     #     raise HTTPException(
     #         status_code=status.HTTP_400_BAD_REQUEST,
     #         detail=ErrorCode.LOGIN_USER_NOT_VERIFIED,
     #     )
 
-    headers = {"HX-Redirect": "/"}
-    response = Response(status_code=status.HTTP_302_FOUND, headers=headers)
-    token = await auth_db_strategy.write_token(user)
-    response = auth_backend.transport._set_login_cookie(response, token)  # type: ignore[attr-defined]
-    return response
+    redirect_response = await log_user_in_and_redirect(auth_db_strategy, user, "/")
+    return redirect_response
