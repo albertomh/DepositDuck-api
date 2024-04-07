0079863054cdf55d5d0a8d5ab59f4580dcd98016
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-07 11:55:24 +0100

    feat(fe): first pass at /signup flow

--- depositduck/auth/routes.py
+++ depositduck/auth/routes.py
@@ -2,12 +2,15 @@
 (c) 2024 Alberto Morón Hernández
 """
 
-from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi import APIRouter, Depends, Form, HTTPException, Request, status
 from fastapi.responses import Response
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_users.authentication.strategy.db import DatabaseStrategy
+from fastapi_users.exceptions import InvalidPasswordException, UserAlreadyExists
 from fastapi_users.router.common import ErrorCode
 from jinja2_fragments.fastapi import Jinja2Blocks
+from pydantic import ValidationError
+from structlog._config import BoundLoggerLazyProxy
 from typing_extensions import Annotated
 
 from depositduck.auth.dependables import (
@@ -16,22 +19,65 @@ from depositduck.auth.dependables import (
     get_user_manager,
 )
 from depositduck.auth.users import auth_backend
-from depositduck.dependables import get_settings, get_templates
-from depositduck.settings import Settings
+from depositduck.dependables import get_logger, get_templates
+from depositduck.models.auth import UserCreate
 
 auth_router = APIRouter()
 
 
-@auth_router.get("/login")
-async def login(
-    settings: Annotated[Settings, Depends(get_settings)],
+@auth_router.get("/signup")
+async def signup(
     templates: Annotated[Jinja2Blocks, Depends(get_templates)],
     request: Request,
 ):
     context = {
         "request": request,
     }
-    return templates.TemplateResponse("login.html.jinja2", context)
+    return templates.TemplateResponse("auth/signup.html.jinja2", context)
+
+
+@auth_router.post("/register", status_code=status.HTTP_201_CREATED)
+async def register(
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    user_manager: Annotated[UserManager, Depends(get_user_manager)],
+    LOG: Annotated[BoundLoggerLazyProxy, Depends(get_logger)],
+    request: Request,
+):
+    try:
+        user_create = UserCreate(email=username, password=password)
+        created_user = await user_manager.create(user_create, safe=True, request=request)
+        LOG.debug(created_user.__dict__)
+    except UserAlreadyExists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=ErrorCode.REGISTER_USER_ALREADY_EXISTS,
+        )
+    except InvalidPasswordException as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "code": ErrorCode.REGISTER_INVALID_PASSWORD,
+                "reason": e.reason,
+            },
+        )
+    except ValidationError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="REGISTER_INVALID_EMAIL",
+        )
+    return Response(status_code=status.HTTP_201_CREATED)
+
+
+@auth_router.get("/login")
+async def login(
+    templates: Annotated[Jinja2Blocks, Depends(get_templates)],
+    request: Request,
+):
+    context = {
+        "request": request,
+    }
+    return templates.TemplateResponse("auth/login.html.jinja2", context)
 
 
 @auth_router.post("/authenticate")
