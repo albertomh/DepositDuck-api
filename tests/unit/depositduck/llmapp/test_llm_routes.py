a8d7fb27978d7218623763ddcb751f5d7ec34b2d
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-15 08:42:22 +0100

    refactor: move unit tests to dedicated directory tests/unit/

--- tests/unit/depositduck/llmapp/test_llm_routes.py
+++ tests/unit/depositduck/llmapp/test_llm_routes.py
@@ -6,24 +6,19 @@ import unittest.mock
 
 import pytest
 
-from depositduck.main import get_webapp
-from tests.conftest import get_aclient
+from depositduck.main import get_llmapp
+from tests.unit.conftest import get_aclient
 
-
-@pytest.mark.asyncio
-async def test_web_root(web_client):
-    response = await web_client.get("/")
-    assert response.status_code == 200
-    # TODO
+# TODO
 
 
 @pytest.mark.asyncio
 async def test_docs_inaccessible_when_debug_false():
     with unittest.mock.patch("depositduck.dependables.get_settings") as mock_get_settings:
         mock_get_settings.return_value.debug = True
-        webapp = get_webapp()
+        llmapp = get_llmapp()
 
-    web_client = await get_aclient(webapp, "http://webtest")
+    web_client = await get_aclient(llmapp, "http://llmtest")
     async with web_client as client:
         response = await client.get("/docs")
         assert response.status_code == 404
