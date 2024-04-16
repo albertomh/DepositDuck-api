af98fa480554e730fff8f21a0a648f0553818727
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-16 23:10:20 +0100

    refactor: simplify testing /docs path hidden when debug=False

--- tests/unit/depositduck/test_main.py
+++ tests/unit/depositduck/test_main.py
@@ -2,16 +2,21 @@
 (c) 2024 Alberto Morón Hernández
 """
 
+import unittest.mock
+
+import pytest
 from starlette.routing import Mount
 
-from depositduck.main import webapp
+from depositduck.main import get_llmapp, get_webapp, webapp
+from tests.unit.conftest import get_aclient
 
 
 def test_app_mounts():
     """
-    Test that mounts for static files, the api & llm apps are present on the webapp.
+    Test that mounts for the `api` & `llm` apps are present on the webapp.
     """
     mounts = {
+        "apiapp": {"path": "/api", "observed": False},
         "llmapp": {"path": "/llm", "observed": False},
     }
 
@@ -23,3 +28,27 @@ def test_app_mounts():
 
     for k, v in mounts.items():
         assert v["observed"], f"{k} not mounted under '{v["path"]}'."
+
+
+@pytest.mark.asyncio
+async def test_web_docs_inaccessible_when_debug_false():
+    with unittest.mock.patch("depositduck.dependables.get_settings") as mock_get_settings:
+        mock_get_settings.return_value.debug = True
+        webapp = get_webapp()
+
+    web_client = await get_aclient(webapp, "http://webtest")
+    async with web_client as client:
+        response = await client.get("/docs")
+        assert response.status_code == 404
+
+
+@pytest.mark.asyncio
+async def test_llm_docs_inaccessible_when_debug_false():
+    with unittest.mock.patch("depositduck.dependables.get_settings") as mock_get_settings:
+        mock_get_settings.return_value.debug = True
+        llmapp = get_llmapp()
+
+    llm_client = await get_aclient(llmapp, "http://llmtest")
+    async with llm_client as client:
+        response = await client.get("/docs")
+        assert response.status_code == 404
