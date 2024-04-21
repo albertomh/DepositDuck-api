59744514a3868bce3690c3bb2ced9ab48b25bb0f
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-21 08:47:41 +0100

    build(test): set e2e config in a dedicated dotenv file

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -2,12 +2,17 @@
 (c) 2024 Alberto Morón Hernández
 """
 
+import os
+
 from enum import StrEnum
 from typing import AsyncGenerator
 
 import pytest_asyncio
 from playwright.async_api import BrowserType, Page, async_playwright
 
+
+HEADLESS: bool = os.getenv("E2E_HEADLESS", "true").lower() == "true" or False
+SLOW_MO = int(os.getenv("E2E_SLOW_MO", 0))  # delay between steps, in milliseconds
 
 class Browser(StrEnum):
     """
@@ -24,7 +29,7 @@ async def browser_page() -> AsyncGenerator[Page, None]:
     async with async_playwright() as playwright:
         browser_choice = Browser.CHROMIUM  # TODO: make parameter
         selected_browser: BrowserType = getattr(playwright, browser_choice.value)
-        browser = await selected_browser.launch()
+        browser = await selected_browser.launch(headless=HEADLESS, slow_mo=SLOW_MO)
         page = await browser.new_page()
 
         try:
