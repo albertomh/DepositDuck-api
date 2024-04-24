10e26168460f608e4e6c724cc538e6b5e8d691f4
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-24 20:16:12 +0100

    feat: add test coverage badge to README

--- depositduck/models/sql/tables.py
+++ depositduck/models/sql/tables.py
@@ -6,7 +6,11 @@ only table models in the alembic `env` module.
 """
 
 # ruff: noqa: F401
-from depositduck.models.sql.auth import AccessToken, User                   # pragma: no cover
-from depositduck.models.sql.email import Email                              # pragma: no cover
-from depositduck.models.sql.llm import EmbeddingNomic, Snippet, SourceText  # pragma: no cover
-from depositduck.models.sql.people import Person, Prospect                  # pragma: no cover
+from depositduck.models.sql.auth import AccessToken, User  # pragma: no cover
+from depositduck.models.sql.email import Email  # pragma: no cover
+from depositduck.models.sql.llm import (  # pragma: no cover
+    EmbeddingNomic,
+    Snippet,
+    SourceText,
+)
+from depositduck.models.sql.people import Person, Prospect  # pragma: no cover
