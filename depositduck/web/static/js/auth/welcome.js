93704bfe8053f069789577f8fbe9fde07e68482c
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-04-30 07:08:06 +0100

    feat: onboarding form can only be submitted once it validates

--- depositduck/web/static/js/auth/welcome.js
+++ depositduck/web/static/js/auth/welcome.js
@@ -5,27 +5,44 @@
 // `/welcome/` - onboarding flow
 function onboardingState(tenancyEndDate) {
     return {
-        name: '',
+        name: "",
         depositAmount: null,
-        tenancyStartDate: '',
-        tenancyEndDate: tenancyEndDate || '',
+        tenancyStartDate: "",
+        tenancyEndDate: tenancyEndDate || "",
         errors: {
             nameIsInvalid: false,
             depositAmountTooSmall: false,
             datesInWrongOrder: false,
             tenancyIsTooShort: false,
         },
-        hasErrors() { Object.values(this.errors).some((value) => value === true); },
+        canSubmitForm: false,
+        hasErrors() {
+            // returns: bool
+            Object.values(this.errors).some((value) => value === true);
+        },
+        allRequiredFieldsHaveValues() {
+            // returns: bool
+            const requiredFields = [
+                this.name, this.depositAmount, this.tenancyStartDate, this.tenancyEndDate
+            ];
+            return !! requiredFields.some(field => field === '' || field === null);
+        },
+        validateName() {
+            // returns: null
+        },
+        validateDepositAmount() {
+            // returns: null
+            this.errors.depositAmountTooSmall = this.depositAmount < 10;
+        },
         daysBetweenDates(date1, date2) {
+            // returns: number
             const diffInMs = date2 - date1;
             const msInDay = 1000 * 60 * 60 * 24;
             const diffInDays = Math.floor(diffInMs / msInDay);
             return diffInDays;
         },
-        validateDepositAmount() {
-            this.errors.depositAmountTooSmall = this.depositAmount < 10;
-        },
         validateTenancyDates() {
+            // returns: null
             if (!this.tenancyStartDate) {
                 return;
             }
@@ -38,8 +55,11 @@ function onboardingState(tenancyEndDate) {
             this.errors.tenancyIsTooShort = (0 < gap < 30);
         },
         validateForm() {
-            this.validateTenancyDates();
+            // returns: null
+            this.validateName()
             this.validateDepositAmount();
+            this.validateTenancyDates();
+            this.canSubmitForm = this.allRequiredFieldsHaveValues() && !this.hasErrors();
         }
     }
 }
