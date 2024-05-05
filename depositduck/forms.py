8b69abe41b2e8f28e3562fd6b02fc8cbefc1cede
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-05-05 19:34:11 +0100

    refactor: make Pydantic BaseForm more flexible

--- depositduck/forms.py
+++ depositduck/forms.py
@@ -4,46 +4,35 @@
 
 from abc import ABC, abstractmethod
 from collections import defaultdict
+from typing import Any
 
-from pydantic import BaseModel, ValidationError
-from pydantic_core import ErrorDetails
+from pydantic import BaseModel
+
+
+class BaseFormFields(BaseModel, ABC):
+    # {"field_name": {"CustomException": "user_input", "AnotherExc": "other_input"}}
+    errors: dict[str, dict[str, str]] = defaultdict(dict)
+
+    def add_error(self, field_name: str, exception: type[Exception], value: Any) -> None:
+        self.errors[field_name][exception.__name__] = value
 
 
 class BaseForm(ABC):
     def __init__(self, **kwargs) -> None:
-        self.can_submit_form = False
-        self.errors: list[ErrorDetails] = []
-        self.field_values: dict = kwargs
-        self.initialize_fields()
+        self.user_input: dict = kwargs
+        form_fields_class = self.get_form_fields_class()
+        self.fields: BaseFormFields = form_fields_class(**self.user_input)
 
     @abstractmethod
-    def get_form_fields_class(self) -> type[BaseModel]:
+    def get_form_fields_class(self) -> type[BaseFormFields]:
         pass
-
-    def initialize_fields(self) -> None:
-        try:
-            form_fields_class = self.get_form_fields_class()
-            form: BaseModel = form_fields_class(**self.field_values)
-            self.field_values = form.model_dump()
-        except ValidationError as e:
-            self.errors = e.errors()
-
-    def get_errors_for_frontend(self) -> dict:
-        errors: dict[str, dict[str, str]] = defaultdict(dict)
-        if self.errors:
-            for error in self.errors:
-                loc = str(error["loc"][0])
-                if self.field_values.get(loc) is not None:
-                    error_cls = type(error["ctx"]["error"]).__name__
-                    errors[loc][error_cls] = error["input"]
-        return errors
 
     def get_classes_for_fields(self) -> dict:
         classes = {}
-        for field_name, value in self.field_values.items():
+        for field_name, value in self.user_input.items():
             class_str = ""
             if value is not None:
-                errors = self.get_errors_for_frontend().get(field_name)
+                errors = self.fields.errors.get(field_name)
                 if errors is None:
                     class_str = "is-valid"
                 else:
@@ -51,10 +40,20 @@ class BaseForm(ABC):
                 classes[field_name] = class_str
         return classes
 
+    @property
+    def can_submit(self) -> bool:
+        return bool(self.user_input) and not bool(self.fields.errors)
+
     def for_template(self) -> dict:
+        fields_export = self.fields.model_dump()
+        errors = fields_export.pop("errors")
+        # templates should not report errors unless there is user input for that field
+        errors = {k: v for k, v in errors.items() if self.user_input.get(k) is not None}
+
         return {
-            "can_submit": self.can_submit_form,
-            "values": self.field_values,
+            "can_submit": self.can_submit,
+            "fields": fields_export,
+            "errors": errors,
+            "user_input": self.user_input,
             "field_classes": self.get_classes_for_fields(),
-            "errors": self.get_errors_for_frontend(),
         }
