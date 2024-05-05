509fc25ec6a977f6fc5695345f0748f1a7676fe0
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-05-05 19:32:23 +0100

    refactor: prospect suitability checks are more composable & reusable

--- tests/unit/depositduck/auth/test_auth_init.py
+++ tests/unit/depositduck/auth/test_auth_init.py
@@ -2,7 +2,7 @@
 (c) 2024 Alberto Morón Hernández
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from unittest.mock import patch
 
 import pytest
@@ -11,8 +11,9 @@ from depositduck.auth import (
     MAX_DAYS_IN_ADVANCE,
     TDS_DISPUTE_WINDOW_IN_DAYS,
     DepositProvider,
-    TenancyEndDateOutOfRange,
-    UnsuitableProvider,
+    DisputeWindowHasClosed,
+    TenancyEndTooFarAway,
+    TooCloseToDisputeWindowEnd,
     is_prospect_suitable,
     send_verification_email,
 )
@@ -22,15 +23,17 @@ TODAY_DATE = datetime.today().date()
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "days_since_end_date",
+    "start_date, end_date",
     [
-        TDS_DISPUTE_WINDOW_IN_DAYS - 10,
-        TDS_DISPUTE_WINDOW_IN_DAYS - (TDS_DISPUTE_WINDOW_IN_DAYS // 2),
-        0 - TDS_DISPUTE_WINDOW_IN_DAYS,
+        (
+            (datetime.today() - timedelta(days=365)).date(),
+            (datetime.today() - timedelta(days=(TDS_DISPUTE_WINDOW_IN_DAYS - 10))).date(),
+        ),
+        ((datetime.today() - timedelta(days=365)).date(), datetime.today().date()),
     ],
 )
-async def test_is_prospect_suitable_happy_path(days_since_end_date):
-    result = await is_prospect_suitable(DepositProvider.TDS.value, days_since_end_date)
+async def test_is_prospect_suitable_happy_path(start_date, end_date):
+    result = await is_prospect_suitable(DepositProvider.TDS.value, start_date, end_date)
 
     assert result is True
 
@@ -41,37 +44,47 @@ async def test_is_prospect_suitable_not_accepted_due_to_provider():
     Invalid prospect due to unsuitable provider, suitable date.
     """
     provider = "invalid_provider"
-    days_since_end_date = TDS_DISPUTE_WINDOW_IN_DAYS // 2
+    start_date = (datetime.today() - timedelta(days=365)).date()
+    end_date = datetime.today().date()
 
-    with pytest.raises(UnsuitableProvider) as exc_info:
-        await is_prospect_suitable(provider, days_since_end_date)
+    with pytest.raises(ExceptionGroup) as exc_info:
+        await is_prospect_suitable(provider, start_date, end_date)
 
-    assert str(exc_info.value) == f"prospect unsuitable due to provider '{provider}'"
+    assert (
+        str(exc_info.value.exceptions[0])
+        == f"prospect unsuitable due to provider '{provider}'"
+    )
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "days_since_end_date",
+    "start_date, end_date, expected_exc_type",
     [
-        TDS_DISPUTE_WINDOW_IN_DAYS + 1,
-        TDS_DISPUTE_WINDOW_IN_DAYS - 3,
+        (
+            (datetime.today() - timedelta(days=365)).date(),
+            (datetime.today() - timedelta(days=(TDS_DISPUTE_WINDOW_IN_DAYS + 1))).date(),
+            DisputeWindowHasClosed,
+        ),
+        (
+            (datetime.today() - timedelta(days=365)).date(),
+            (datetime.today() - timedelta(days=(TDS_DISPUTE_WINDOW_IN_DAYS - 3))).date(),
+            TooCloseToDisputeWindowEnd,
+        ),
     ],
 )
 async def test_is_prospect_suitable_not_accepted_due_to_outside_dispute_window(
-    days_since_end_date,
+    start_date, end_date, expected_exc_type
 ):
     """
     Invalid prospect due to suitable provider, today falling either:
       - outside (tenancy end date + dispute window).
       - between (dispute_window_end - 5) and dispute_window_end
     """
-    with pytest.raises(TenancyEndDateOutOfRange) as exc_info:
-        await is_prospect_suitable(DepositProvider.TDS.value, days_since_end_date)
+    with pytest.raises(ExceptionGroup) as exc_info:
+        await is_prospect_suitable(DepositProvider.TDS.value, start_date, end_date)
 
-    assert (
-        str(exc_info.value)
-        == f"prospect unsuitable due to end date being {days_since_end_date} days ago"
-    )
+    exc = exc_info.value.exceptions[0]
+    assert isinstance(exc, expected_exc_type)
 
 
 @pytest.mark.asyncio
@@ -80,16 +93,14 @@ async def test_is_prospect_suitable_not_accepted_due_to_over_six_months_away():
     Invalid prospect due to suitable provider, unsuitable tenancy end date due to being
     over six_months away.
     """
-    days_until_end_date = (-1 * MAX_DAYS_IN_ADVANCE) - 20
+    start_date = datetime.today().date()
+    end_date = (datetime.today() + timedelta(days=(MAX_DAYS_IN_ADVANCE + 1))).date()
 
-    with pytest.raises(TenancyEndDateOutOfRange) as exc_info:
-        await is_prospect_suitable(DepositProvider.TDS.value, days_until_end_date)
+    with pytest.raises(ExceptionGroup) as exc_info:
+        await is_prospect_suitable(DepositProvider.TDS.value, start_date, end_date)
 
-    exc_msg = (
-        f"prospect unsuitable due to end date being {days_until_end_date * -1} "
-        "days from now"
-    )
-    assert str(exc_info.value) == exc_msg
+    exc = exc_info.value.exceptions[0]
+    assert isinstance(exc, TenancyEndTooFarAway)
 
 
 @pytest.mark.asyncio
