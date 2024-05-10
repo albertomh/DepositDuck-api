773d822226c1725c6820f592dea32b5f8c14211f
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-05-10 07:25:00 +0100

    refactor: prospect form validation returns per-field markup

--- depositduck/web/templates/__init__.py
+++ depositduck/web/templates/__init__.py
@@ -1,12 +1,3 @@
 """
 (c) 2024 Alberto Morón Hernández
 """
-
-from enum import Enum
-
-
-# TODO: remove and refactor all uses to use Alpine.js
-class BootstrapClasses(str, Enum):
-    FORM_CONTROL = "form-control"
-    IS_VALID = "is-valid"
-    IS_INVALID = "is-invalid"
